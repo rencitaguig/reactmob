619fe2d05644cacc586a9b406430d8d40fa28897
rencitaguig <renecianemanuel.baloloy@tup.edu.ph>
2025-03-30 13:43:36 +0800

    added field: user role

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   profileImage: { type: String }, // Add profileImage field
+  role: { type: String, enum: ["user", "admin"], default: "user" }, // Add role field with default value
 });
 
 // Hash password before saving
